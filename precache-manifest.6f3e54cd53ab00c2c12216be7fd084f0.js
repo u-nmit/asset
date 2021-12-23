7879044b916d693d4956af72de05577f3609019e
Taivanjargal11 <s18ud1101039@nmit.edu.mn>
2021-12-24 06:46:11 +0800

    Updates

---
+++ precache-manifest.6f3e54cd53ab00c2c12216be7fd084f0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe4f2d19df017b87e015de61cf57311",
+    "url": "/asset/index.html"
+  },
+  {
+    "revision": "0ed85f133a7a643f6e26",
+    "url": "/asset/static/css/3.6165105e.chunk.css"
+  },
+  {
+    "revision": "e0f3102d653f94219ed4",
+    "url": "/asset/static/css/4.81e11b69.chunk.css"
+  },
+  {
+    "revision": "8f4e9f4a4e53adc79aff",
+    "url": "/asset/static/css/5.6254e648.chunk.css"
+  },
+  {
+    "revision": "0f8bd99a741997f5dd26",
+    "url": "/asset/static/css/main.485f90c3.chunk.css"
+  },
+  {
+    "revision": "10df2043657cdea2e3e3",
+    "url": "/asset/static/js/2.535b2ff4.chunk.js"
+  },
+  {
+    "revision": "0d08bb566150086a6317785a01bb3007",
+    "url": "/asset/static/js/2.535b2ff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ed85f133a7a643f6e26",
+    "url": "/asset/static/js/3.c4e3d2a0.chunk.js"
+  },
+  {
+    "revision": "e0f3102d653f94219ed4",
+    "url": "/asset/static/js/4.e131f020.chunk.js"
+  },
+  {
+    "revision": "8f4e9f4a4e53adc79aff",
+    "url": "/asset/static/js/5.8612bd26.chunk.js"
+  },
+  {
+    "revision": "0f8bd99a741997f5dd26",
+    "url": "/asset/static/js/main.f66f26d2.chunk.js"
+  },
+  {
+    "revision": "7841bdfdc11e86063779",
+    "url": "/asset/static/js/runtime-main.20a85985.js"
+  },
+  {
+    "revision": "4fd0e1982cb5b77f6902ce271f1221b7",
+    "url": "/asset/static/media/logoNmit.4fd0e198.png"
+  },
+  {
+    "revision": "aa2e0bc776d400e7db5fa30078136afb",
+    "url": "/asset/static/media/nmit.aa2e0bc7.mp4"
+  }
+]);
